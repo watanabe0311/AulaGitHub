b62046fe33aad9d34487ade77607c7a4ebb1b7d7
watanabe0311 <165096829+watanabe0311@users.noreply.github.com>
2024-03-27 09:33:38 +0000

    Nome calc

--- CalculadoraTeste.java
+++ CalculadoraTeste.java
@@ -3,16 +3,16 @@ import javax.swing.JOptionPane;
 public class CalculadoraTeste {
     
     public static void main(String[] args) {
-        Calculadora calvoguedes = new Calculadora(Double.parseDouble(JOptionPane.showInputDialog("digite seu numero preferido ")));
+        Calculadora calc = new Calculadora(Double.parseDouble(JOptionPane.showInputDialog("digite seu numero preferido ")));
         //testar soma
-        double soma = calvoguedes.somar(2, 3);
+        double soma = calc.somar(2, 3);
         System.out.println(soma);
         //testar subtracao
-        double sub = calvoguedes.subtrair(2, 3);
+        double sub = calc.subtrair(2, 3);
         System.out.println(sub);
 
-        System.out.println(calvoguedes.somarComPreferido(Double.parseDouble(JOptionPane.showInputDialog("digite seu numero para somar"))));
+        System.out.println(calc.somarComPreferido(Double.parseDouble(JOptionPane.showInputDialog("digite seu numero para somar"))));
 
-        System.out.println(calvoguedes.subtrair(10, 3));
+        System.out.println(calc.subtrair(10, 3));
     }
 }
