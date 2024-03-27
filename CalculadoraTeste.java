df939a9c618d0fe0b5d77429269f105b173281f3
Henrique Watanabe <henrique28.dw@gmail.com>
2024-03-26 21:13:42 -0300

    chage log

--- CalculadoraTeste.java
+++ CalculadoraTeste.java
@@ -1,12 +1,18 @@
+import javax.swing.JOptionPane;
+
 public class CalculadoraTeste {
     
     public static void main(String[] args) {
-        Calculadora calc = new Calculadora();
+        Calculadora calvoguedes = new Calculadora(Double.parseDouble(JOptionPane.showInputDialog("digite seu numero preferido ")));
         //testar soma
-        double soma = calc.somar(2, 3);
+        double soma = calvoguedes.somar(2, 3);
         System.out.println(soma);
         //testar subtracao
-        double sub = calc.subtrair(2, 3);
+        double sub = calvoguedes.subtrair(2, 3);
         System.out.println(sub);
+
+        System.out.println(calvoguedes.somarComPreferido(Double.parseDouble(JOptionPane.showInputDialog("digite seu numero para somar"))));
+
+        System.out.println(calvoguedes.subtrair(10, 3));
     }
 }
